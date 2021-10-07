a8024decbbffbb0fa4b2f3ea323d8d55a3fb6b15
Scott Rutz <scrutz@microsoft.com>
2021-10-07 11:59:48 -0500

    cp

--- Demo.Function.Movies.API/CategoryChangeFeedActivity.cs
+++ Demo.Function.Movies.API/CategoryChangeFeedActivity.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+
+using Demo.Function.Movies.Api.Data;
 
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
 
 using StackExchange.Redis;
 
@@ -14,24 +19,25 @@ namespace Demo.Function.Movies.API
     public class CategoryChangeFeedActivity
     {
         private IConnectionMultiplexer _redisConnectionMultiplexer;
+        private CosmosClient _cosmosClient;
 
-        public CategoryChangeFeedActivity(IConnectionMultiplexer redisConnectionMultiplexer)
+        public CategoryChangeFeedActivity(IConnectionMultiplexer redisConnectionMultiplexer, CosmosClient cosmosClient)
         {
             this._redisConnectionMultiplexer = redisConnectionMultiplexer;
+            _cosmosClient = cosmosClient;
         }
 
         [FunctionName("CategoryChangeFeedActivity")]
-        public void Run([CosmosDBTrigger(databaseName: "MoviesDB", collectionName: "Category", ConnectionStringSetting = "CosmosDBConnection", LeaseCollectionName = "leases")] IReadOnlyList<Document> input, ILogger log)
+        public void Run([CosmosDBTrigger(databaseName: "MoviesDB", collectionName: "Category", ConnectionStringSetting = "CosmosDBConnection", LeaseCollectionName = "leases", StartFromBeginning = true)] IReadOnlyList<Document> input, ILogger log)
         {
             if (input == null || input.Count <= 0)
                 return;
 
+            var qs = new MovieQueryService(_cosmosClient);
+            var activeCategories = qs.GetCategories().Result.Result.Where(x => x.IsActive);
+
             var db = _redisConnectionMultiplexer.GetDatabase();
-            foreach (var document in input)
-            {
-                db.StringSet(document.Id, document.ToString());
-                log.LogInformation($"Saved item with id {input.Count} in Azure Redis cache");
-            }
+            db.StringSet("CategoryCache", JsonConvert.SerializeObject(activeCategories));
         }
     }
 }
--- Demo.Function.Movies.API/Data/Movie.cs
+++ Demo.Function.Movies.API/Data/Movie.cs
@@ -12,5 +12,7 @@ namespace Demo.Function.Movies.API.Data
         public string CategoryName { get; set; }
 
         public string Description { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }
